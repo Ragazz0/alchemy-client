2236d750e5bfae683cf20a7caa8db174881dc2d0
Gang Li <leemagpie@gmail.com>
2015-03-04 15:35:11 -0500

    change to use fixed length diff

--- align/align.py
+++ align/align.py
@@ -1,72 +1,89 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-import re
-import codecs
 
-from submodules.alignment.alignment import Alignment,Hirschberg
-from submodules.annotation.readers import AnnReader
-from submodules.annotation.writers import AnnWriter
+from submodules.alignment.alignment import SegmentAlignment
 
-writer = AnnWriter()
 
-def get_phrase(text):
-    p = re.compile(r'[a-zA-Z]+|[0-9]+|\s+|[.,;!\(\)]+')
-    lista = []
-    pre = 0
-    for m in p.finditer(text):
-        start = m.start()
-        end = m.end()
-        if pre < start:
-            lista.append(text[pre:start])
-        lista.append(text[start:end])
-        pre = end
-    return lista
+class AnnotationAligner(object):
+    aligner = SegmentAlignment()
 
-for root,_,files in os.walk('input'):
-    for f in files:        
-        if not f.endswith('.txt'):
-            continue
-        pmid = f[:-4]
-        print(pmid)
-        alter = os.path.join(root,pmid+'.txt')
-        alterFile = codecs.open(alter,'r','utf-8')
-        alterText = alterFile.read().strip()
-        alterFile.close()
+    @classmethod
+    def align(cls, annotation, altered_text, original_text):
 
-        reader = AnnReader(root,pmid+'.ann')
-        annotation = reader.parse()
-        
-        if len(annotation['T']) == 0:
-            writer.write('output',pmid+'.ann',annotation)
-            continue
+        aligned_gold, aligned_altered = cls.aligner.align(original_text, altered_text,
+                                                          segment_half=True, base_alignment='Hirschberg')
+        alter2gold = cls.aligner.map_alignment(''.join(aligned_gold), ''.join(aligned_altered))
 
-        gold = os.path.join('output',pmid+'.txt')
-        goldFile = codecs.open(gold,'r','utf-8')
-        goldText = goldFile.read().strip()
-        goldFile.close()
+        for entity in annotation.entities:
+            start = int(entity.start)
+            end = int(entity.end)
 
-        entities = annotation['T']
-
-        goldPhrases = get_phrase(goldText)
-        alterPhrases = get_phrase(alterText)
-        h = Hirschberg(goldPhrases,alterPhrases)
-        #h = Hirschberg(list(goldText),list(alterText))
-        alignGold,alignAlter = h.align()
-        #print ''.join(alignGold)
-        #print ''.join(alignAlter)
-        alter2gold = h.map_alignment(''.join(alignGold),''.join(alignAlter))
-        
-        for k,e in entities.iteritems():    
-            start = int(e.start)
-            end = int(e.end)
-
-            e.start = alter2gold[start]
-            if alter2gold[end] - alter2gold[end-1] > 1:
-                e.end = alter2gold[end-1]+1
+            entity.start = alter2gold[start]
+            if alter2gold[end] - alter2gold[end - 1] > 1:
+                entity.end = alter2gold[end - 1] + 1
             else:
-                e.end = alter2gold[end]
-            e.text = goldText[e.start:e.end]
+                entity.end = alter2gold[end]
+            entity.text = original_text[entity.start:entity.end]
+        
+        return annotation
 
-        writer.write('output',pmid+'.ann',annotation)
+# for root, _, files in os.walk('input'):
+#     for f in files:
+#         if not f.endswith('.txt'):
+#             continue
+#         pmid = f[:-4]
+#         print(pmid)
+#         alter = os.path.join(root, pmid + '.txt')
+#         alterFile = codecs.open(alter, 'r', 'utf-8')
+#         alterText = alterFile.read().strip()
+#         alterFile.close()
+#
+#         reader = AnnReader(root, pmid + '.ann')
+#         annotation = reader.parse()
+#
+#         if len(annotation['T']) == 0:
+#             writer.write('output', pmid + '.ann', annotation)
+#             continue
+#
+#         gold = os.path.join('output', pmid + '.txt')
+#         goldFile = codecs.open(gold, 'r', 'utf-8')
+#         goldText = goldFile.read().strip()
+#         goldFile.close()
+#
+#         entities = annotation['T']
+#
+#         goldPhrases = get_phrase(goldText)
+#         alterPhrases = get_phrase(alterText)
+#         h = Hirschberg(goldPhrases, alterPhrases)
+#         # h = Hirschberg(list(goldText),list(alterText))
+#         alignGold, alignAlter = h.align()
+#         #print ''.join(alignGold)
+#         #print ''.join(alignAlter)
+#         alter2gold = h.map_alignment(''.join(alignGold), ''.join(alignAlter))
+#
+#         for k, e in entities.iteritems():
+#             start = int(e.start)
+#             end = int(e.end)
+#
+#             e.start = alter2gold[start]
+#             if alter2gold[end] - alter2gold[end - 1] > 1:
+#                 e.end = alter2gold[end - 1] + 1
+#             else:
+#                 e.end = alter2gold[end]
+#             e.text = goldText[e.start:e.end]
+#
+#         writer.write('output', pmid + '.ann', annotation)
 
+        # writer = AnnWriter()
+        #
+        # def get_phrase(text):
+        #     p = re.compile(r'[a-zA-Z]+|[0-9]+|\s+|[.,;!\(\)]+')
+        #     lista = []
+        #     pre = 0
+        #     for m in p.finditer(text):
+        #         start = m.start()
+        #         end = m.end()
+        #         if pre < start:
+        #             lista.append(text[pre:start])
+        #         lista.append(text[start:end])
+        #         pre = end
+        #     return lista
