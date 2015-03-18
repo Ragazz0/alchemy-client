5ee535aefa1e131cd192ef932d40f56cec0040b3
Gang Li <leemagpie@gmail.com>
2015-03-18 12:00:38 -0400

    add miRTex reader

---
+++ client/processors/mirtex.py
@@ -0,0 +1,64 @@
+import os
+from submodules.annotation.readers import AnnParser
+from submodules.annotation.utils import FileProcessor
+import json
+
+# required suffix
+SUFFIX = ('.ann', '.txt')
+
+# entity categories
+ENTITY_CATEGORY = ('Gene', 'MiRNA')
+
+# relation categories
+RELATION_CATEGORY = (('MiRNA2Gene', ('Trigger', 'Agent', 'Theme')),
+                     ('Gene2MiRNA', ('Trigger', 'Agent', 'Theme')))
+
+
+def handler(relation, fields):
+    if len(fields) == 0:
+        return
+    json_str = fields[0]
+    json_obj = json.loads(json_str)
+    for key, value in json_obj.items():
+        if isinstance(value, list):
+            relation.property[key] = value[0]
+        else:
+            relation.property[key] = value
+
+def process(filepath):
+    parser = AnnParser(event_handler=handler, relation_handler=handler)
+    annotations = []
+
+    # read annotation
+    ann_file = filepath + '.ann'
+    annotation = parser.parse_file(ann_file)
+
+    # remove sentence entity
+    annotation.entities = [e for e in annotation.entities if e.category != 'Sentence']
+
+    for relation in annotation.relations:
+        relation.category = 'MiRNA2Gene'
+        if 'direction' in relation.property:
+            if relation.property.get('direction') == 'G2M':
+                relation.category = 'Gene2MiRNA'
+            del relation.property['direction']
+        
+        for arg in relation.arguments:
+            if arg.role == 'Arg1':
+                arg.role = 'Agent'
+            elif arg.role == 'Arg2':
+                arg.role = 'Theme'
+        
+    # read text
+    text_file = filepath + '.txt'
+    text = FileProcessor.read_file(text_file)
+
+    # store text with annotation
+    annotation.text = text
+
+    # get doc_id
+    doc_id = os.path.basename(filepath)
+    annotation.doc_id = doc_id
+    
+    annotations.append(annotation.pack())
+    return annotations
