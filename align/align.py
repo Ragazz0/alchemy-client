30fede0fcb397ba002c4631ae500d82543372229
Gang Li <leemagpie@gmail.com>
2015-03-18 18:27:19 -0400

    fix alignment bug

--- align/align.py
+++ align/align.py
@@ -17,19 +17,19 @@ class AnnotationAligner(object):
         :rtype: None
         """
         altered_text = annotation.get('text')
-        
+
         # base_alginment = Hirschberg, segment_half = True, segment = 50, diff = 50
         aligned_gold, aligned_altered = cls.aligner.align(original_text, altered_text,
                                                           segment_half=True, base_alignment='Hirschberg')
 
         alter2gold = cls.aligner.map_alignment(''.join(aligned_gold), ''.join(aligned_altered))
-        
+
         for entity in annotation.get('entity_set'):
             start = int(entity.get('start'))
             end = int(entity.get('end'))
 
-            entity['start'] = alter2gold[start]
             try:
+                entity['start'] = alter2gold[start]
                 if end >= len(alter2gold):
                     # end is an index in a range, so it could
                     # equal to the length of length of the altered string
@@ -40,7 +40,7 @@ class AnnotationAligner(object):
                     entity['end'] = alter2gold[end]
             except IndexError:
                 traceback.print_exc()
-                print(annotation.get('doc_id'), len(alter2gold),start,end, sep="\t")
+                print(annotation.get('doc_id'), len(alter2gold), start, end, sep="\t")
             entity['text'] = original_text[entity.get('start'):entity.get('end')]
-        
+
         annotation['text'] = original_text
