1a8ef6e923463936e66a61d4be4792683b91425c
Gang Li <leemagpie@gmail.com>
2015-03-23 11:50:45 -0400

    filter out FP miRTex results

--- client/processors/mirtex.py
+++ client/processors/mirtex.py
@@ -13,6 +13,8 @@ ENTITY_CATEGORY = ('Gene', 'MiRNA', 'Trigger')
 RELATION_CATEGORY = (('MiRNA2Gene', ('Trigger', 'Agent', 'Theme')),
                      ('Gene2MiRNA', ('Trigger', 'Agent', 'Theme')))
 
+remove_needle = {'luciferase reporter', 'luciferase reporters', 'luciferase'}
+
 
 def handler(relation, fields):
     if len(fields) == 0:
@@ -24,6 +26,7 @@ def handler(relation, fields):
             relation.property[key] = value[0]
         else:
             relation.property[key] = value
+
 
 def process(filepath):
     parser = AnnParser(event_handler=handler, relation_handler=handler)
@@ -37,30 +40,36 @@ def process(filepath):
     doc_id = os.path.basename(filepath)
     annotation.doc_id = doc_id
     annotation.filepath = filepath
-    
+
     # remove sentence and certain entities
     annotation.entities = [e for e in annotation.entities if e.category != 'Sentence']
-    to_remove_entity = [e for e in annotation.entities if e.text.lower() == 'luciferase reporter']
+    to_remove_entity = [e for e in annotation.entities if e.text.lower() in remove_needle]
     annotation.entities = [e for e in annotation.entities if e not in to_remove_entity]
     to_remove_relation = []
-    
+
     for relation in annotation.relations:
         relation.category = 'MiRNA2Gene'
         if 'direction' in relation.property:
             if relation.property.get('direction') == 'G2M':
                 relation.category = 'Gene2MiRNA'
             del relation.property['direction']
-        
+
         for arg in relation.arguments:
             if arg.role == 'Arg1':
                 arg.role = 'Agent'
             elif arg.role == 'Arg2':
                 arg.role = 'Theme'
-                
+
             if arg.value in to_remove_entity:
                 to_remove_relation.append(relation)
     
+    # remove relations and trigger words
     annotation.relations = [r for r in annotation.relations if r not in to_remove_relation]
+    to_remove_trigger = [a.value for r in to_remove_relation for a in r.arguments if a.role == 'Trigger']
+    annotation.entities = [e for e in annotation.entities if e not in to_remove_trigger]
+
+    if len(annotation.relations) == 0:
+        return annotations
 
     # read text
     text_file = filepath + '.txt'
@@ -68,6 +77,6 @@ def process(filepath):
 
     # store text with annotation
     annotation.text = text
-    
+
     annotations.append(annotation.pack())
     return annotations
